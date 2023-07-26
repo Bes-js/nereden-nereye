cc78a2d7872d7e2c72e98c577d6d28a1ef253a86
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2023-07-26 18:40:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ const app = express();
 import ejs from 'ejs';
 import path from 'path';
 import bodyParser from "body-parser";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine("ejs", ejs.renderFile);
 app.set("view engine", "ejs");
-app.set("views", path.join("views"));
+app.set("views", path.join(__dirname,"views"));
 
-app.use("/public/", express.static(path.join("public")));
+app.use("/public/", express.static(path.join(__dirname,"public")));
 
 app.get('/ne-kadar-sikildin',async(_,res) => {
   res.render("index");
