28aa97756d3f5cb08d2510cce926a1e08cb8fa49
Arda Öztürk <relivent.social@gmail.com>
2023-07-27 17:53:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,28 +11,24 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.engine("ejs", ejs.renderFile);
+  app.set("view engine", "ejs");
+  app.set("views", path.join(__dirname, "views"));
+  app.use("/public/", express.static(path.join(__dirname, "public")));
 
-app.engine("ejs", ejs.renderFile);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"views"));
-
-app.use("/public/", express.static(path.join(__dirname,"public")));
-
-app.get('/ne-kadar-sikildin',async(_,res) => {
+["/", "/ne-kadar-sikildin"].forEach(function(route) {
+app.get(route, function(_, res) {
   res.render("index");
 });
-
-app.get("/", function(_, res) {
-  res.render("index");
 });
 
 app.get("/api", async function(_, res) {
-var date = Math.floor(Date.now() - new Date("2001-08-14").getTime());
+const date = Math.floor(Date.now() - new Date("2001-08-14").getTime());
 try {
-var res2 = await axios.get(`https://uzmanpara.milliyet.com.tr/dolar-kuru/`);
-var $ = cheerio.load(`${res2.data}`);
+const res2 = await axios.get(`https://uzmanpara.milliyet.com.tr/dolar-kuru/`);
+const $ = cheerio.load(`${res2.data}`);
 const data = { yuzde: $(`#doviz_tbl_1 > tbody > tr:nth-child(2) > td.degisim.up`).text(), kur: $(`#doviz_tbl_1 > tbody > tr:nth-child(2) > td:nth-child(3)`).text() };
   res.json({ message: `${moment.duration(date).format("y [Yıl], d [Gün], h [Saat], m [Dakika], s [Saniye]")}'dir ${data.kur} TL İle ${data.yuzde} Oranında S*k*l*yorsun...` })
 } catch(err) {
